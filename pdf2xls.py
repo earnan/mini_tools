b2ba6a51679c5b2d53a9272ed0b7fab0dff3a8aa
earnan <hi@arcsona.cn>
2022-04-21 17:47:21 +0800

    1

--- pdf2xls.py
+++ pdf2xls.py
@@ -1,3 +1,13 @@
+
+import pdfplumber
+
+with pdfplumber.open("F:/test.pdf") as pdf:
+    page = pdf.pages[2]  # 第三页
+    print(page.extract_table())
+    for row in page.extract_table():
+        print(row)
+
+"""
 # 方法①
 import tabula
 import camelot
@@ -5,7 +15,7 @@ import camelot
 tables = camelot.read_pdf("F:/test.pdf")
 print(tables)
 tables.export("F:/extracted.csv", f="csv", compress=True)
-"""
+
 # 方法②, 需要安装Java8
 
 tabula.read_pdf("tables.pdf", pages="all")
